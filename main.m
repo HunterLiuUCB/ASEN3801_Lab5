1dec9872d840d256ce90755c6f6b3d3e2be6b221
HunterLiuUCB <huli3897@colorado.edu>
2026-04-07 08:46:10 -0600

    Initial files

---
+++ main.m
@@ -0,0 +1,16 @@
+%% Main.m
+
+
+%% AircraftEOM.m Functin
+% Inputs:
+%   - time
+%   - aircraft_state - 12x1 state vector
+%   - aircraft_surfaces - 4x1 control surface vector
+%   - wind_inertial - 3x1 wind velocity in inertial coordinates
+%   - aircraft_parameters - aircraft parameter structure
+
+
+function xdot = AircraftEOM[time, aircraft_state, aircraft_surfaces, wind_inertial,
+aircraft_parameters]
+
+end
