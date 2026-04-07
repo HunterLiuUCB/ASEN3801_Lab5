fd254d5058721b9dd4928749eac5e21356a47175
cayleighmorton <camo4668@colorado.edu>
2026-04-07 10:30:35 -0600

    Update main.m

--- main.m
+++ main.m
@@ -19,3 +19,14 @@ tspan = [0 10];
 initial_state_22 = [0; 0; -1800; 0; 0.02780; 0; 20.99; 0; 0.5837; 0; 0; 0];
 aircraft_surfaces_22 = [0.1079; 0; 0; 0.3182];
 [t_out_22, state_out_22] = ode45(@(time, aircraft_state) AircraftEOM(time, aircraft_state, aircraft_surfaces_22, wind_inertial, aircraft_parameters), tspan, initial_state_22);
+
+%% 2.3
+con2rad = pi/180;
+initial_state23 = [0;   0;    -1800; 
+                15*con2rad; -12*con2rad; 270*con2rad; 
+                19; 3; -2; 
+                0.08*con2rad; -0.2*con2rad; 0];
+%aircraft_surfaces = [de da dr dt]; 
+aircraft_surfaces23 = con2rad*[5; 2; -13; 0.3];
+
+[t_out_23, state_out_23] = ode45(@(time, aircraft_state) AircraftEOM(time, aircraft_state, aircraft_surfaces23, wind_inertial, aircraft_parameters), tspan, initial_state23);
