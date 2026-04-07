0c8ae9a2bd6f3da878e1c9ec59a5950c22628f9f
HunterLiuUCB <huli3897@colorado.edu>
2026-04-07 09:09:06 -0600

    Update main.m

--- main.m
+++ main.m
@@ -1,5 +1,18 @@
 %% Main.m
+clear
+clc
+close all
+% stdatmo.m
 
+ttwistor;
+
+%% 2.1
+h = 1609.34; %m
+%density = stdatmo(h); % NEED FUNCTION MATHWORKS IS CURRENTLY DOWN
+aircraft_state = [0; 0; 0; 0; 0; 0; 21; 0; 0; 0; 0; 0];
+%aircraft_surfaces = [de da dr dt]; 
+aircraft_surfaces = [0; 0; 0; 0];
+wind_inertial = [0; 0; 0];
 
 %% AircraftEOM.m Functin
 % Inputs:
@@ -9,8 +22,6 @@
 %   - wind_inertial - 3x1 wind velocity in inertial coordinates
 %   - aircraft_parameters - aircraft parameter structure
 
-
-function xdot = AircraftEOM[time, aircraft_state, aircraft_surfaces, wind_inertial,
-aircraft_parameters]
-
+function xdot = AircraftEOM(time, aircraft_state, aircraft_surfaces, wind_inertial, aircraft_parameters)
+    [aero_forces, aero_moments] = AeroForcesAndMoments(aircraft_state, aircraft_surfaces, wind_inertial, density, aircraft_parameters);
 end
